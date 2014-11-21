20a235203bc4c4064a85f49745920166916e4153
Jamis Johnson <jamismanwaring@gmail.com>
2014-11-21 13:12:51 -0500

    the marrieds

--- xmas.py
+++ xmas.py
@@ -1,5 +1,5 @@
 from random import shuffle
-family = ["Mom", "Clark", "Adrian", "Lauren", "Pace", "Jamey", "Ella"]
+family = ["Mom", "Clark/Bethany", "Adrian", "Lauren/Brian", "Pace/Stacy", "Jamey", "Ella"]
 shuffle(family)
 family.append(family[0]) # presents circular behavior better
 print " -> ".join(family)
